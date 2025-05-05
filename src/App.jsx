c6c2f1e653a7160cff143b46be175bb37362d517
RUSHAID-MUHAMMED-AM <rushaidmuhammedam@gmail.com>
2025-05-05 11:08:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Hello from './components/Hello.jsx';
+
+function App() {
+
+  return (
+    <>
+
+ <Hello name="Rushaid" message="Hello"/>
+    </>
+  )
+}
+
+export default App
